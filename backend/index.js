cbff9563eb390265b235ac5cccc1a26ba601bf18
Muhamad Akbar Afriansyah <itsme@akbarafriansyah.my.id>
2023-09-11 06:18:42 +0000

    setup mongo

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,24 @@ const cors = require('cors');
 const app = express()
 require("dotenv").config();
 const port = process.env.PORT
+const dbatlas = process.env.DBATLAS
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+
+// Connect to MongoDB Atlas
+mongoose.connect(
+        dbatlas,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB Atlas connection established successfully!');
+});
 
 app.get('/', (req, res) => {
     res.send('Home')
